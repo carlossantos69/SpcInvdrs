aabbecd370bda295a5ddab0a035cee1143f3cb47
Tomás Corral <tomas.corral@tecnico.ulisboa.pt>
2025-01-06 15:41:37 +0000

    Moved client logic to seperate file for future astronaut-client-display.c creation

---
+++ Part B/src/client-logic.h
@@ -0,0 +1,23 @@
+/*
+ * PSIS 2024/2025 - Project Part 1
+ *
+ * Filename: client-logic.h
+ *
+ * Authors:
+ * - Carlos Santos - 102985 - carlos.r.santos@tecnico.ulisboa.pt
+ * - Tomas Corral  - 102446 - tomas.corral@tecnico.ulisboa.pt
+ *
+ * Group ID: 20
+ *
+ * Description:
+ * Header file for client-logic.h
+ */
+
+
+#ifndef CLIENT_LOGIC_H
+#define CLIENT_LOGIC_H
+
+
+void client_main(void* req);
+
+#endif
