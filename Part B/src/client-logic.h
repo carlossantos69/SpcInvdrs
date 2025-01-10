a419d7e1a42bed8780fceea1b56426269a71773d
Tomás Corral <tomas.corral@tecnico.ulisboa.pt>
2025-01-10 17:40:58 +0000

    client-logic.h codee cleaning

--- Part B/src/client-logic.h
+++ Part B/src/client-logic.h
@@ -16,11 +16,33 @@
 
 #ifndef CLIENT_LOGIC_H
 #define CLIENT_LOGIC_H
+
 #include "config.h"
 #include "pthread.h"
 
+/**
+ * @brief Handles input key events.
+ *
+ * This function locks the client mutex, sets the input character and marks it as ready,
+ * signals the condition variable to notify other threads, and then unlocks the mutex.
+ *
+ * It is used by main programs to send characters to the client logic
+ * 
+ * @param ch The input character to be processed.
+ */
 void input_key(int ch);
 
+/**
+ * @brief Main function for the client logic.
+ *
+ * This function initializes the necessary synchronization primitives, sends a connect message to the server,
+ * and enters the main client loop to handle key inputs. It uses a mutex and condition variable to synchronize
+ * input handling. The function will exit if there is a failure in initialization, connection, or if the key
+ * input handling indicates to stop.
+ *
+ * @param requester A pointer to the requester object.
+ * @param ncurses An integer flag indicating whether ncurses mode is enabled.
+ */
 void client_main(void* requester, int ncurses);
 
 #endif
